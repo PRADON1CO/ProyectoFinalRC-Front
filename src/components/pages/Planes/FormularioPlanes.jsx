e9b06dffffe6433d8c8ec24abb8a0c226203a736
Esteban Lopez <lopezestebanalejandro@gmail.com>
2024-06-12 21:01:37 -0300

    Se agrego sweetalert2 y se crea componente en carpeta planes

---
+++ src/components/pages/Planes/FormularioPlanes.jsx
@@ -0,0 +1,14 @@
+import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { crearProducto, editarProducto, obtenerProducto } from "../../helpers/queries.js";
+import Swal from "sweetalert2";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const DetalleDelPlan = () => {
+  return (
+    dsds
+  );
+};
+
+export default DetalleDelPlan;
