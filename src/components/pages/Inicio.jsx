ace01589cc8b934e377232181fe1f3076e5c3c66
Prado Nicolas <nicolas88prado@gmail.com>
2024-06-14 22:42:49 -0300

    agregue el banner en el componente Inicio.jsx

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { Button, Container } from "react-bootstrap";
+import fondo1 from "../../assets/fondo1.webp" 
 
 const Inicio = () => {
     return (
         <div className="mainSection">
-        
+            <div className='banner text-center p-1'>
+                <h3 className="">Transforma tu cuerpo. Mejora tu vida con nocotros.</h3>
+                <button className="p-1 rounded-2 bgVerde mt-2 text-white">Comenzar Ahora</button>
+            </div>
+            <Container>
+                
+            </Container>
         </div>
     );
 };
