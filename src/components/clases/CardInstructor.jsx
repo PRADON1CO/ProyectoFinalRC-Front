e16afe12c2556237725c8bcf43f2acaed21de253
Prado Nicolas <nicolas88prado@gmail.com>
2024-06-25 22:26:44 -0300

    agre el read para los componetes CradInstructor y CardClases

--- src/components/clases/CardInstructor.jsx
+++ src/components/clases/CardInstructor.jsx
@@ -1,16 +1,16 @@
 
 import { Col } from 'react-bootstrap';
 
-const CardInstructor = () => {
+const CardInstructor = ({instructor}) => {
     return (
         <Col md={5} lg={4} className="pt-2">
             <div className="text-center">
                 <img 
-                    src="https://images.pexels.com/photos/3756907/pexels-photo-3756907.jpeg?auto=compress&cs=tinysrgb&w=300" 
-                    alt="Foto del Instructor" 
-                    className="rounded-circle"
+                    src={instructor.imagen}
+                    alt={instructor.nombreProfesor}
+                    className="rounded-circle imgInstructor"
                 />
-                <h4 className='pt-1'>Lourdes Garcia</h4>
+                <h4 className='pt-1'>{instructor.nombreProfesor}</h4>
             </div>
         </Col>
     );
